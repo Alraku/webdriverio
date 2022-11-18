0fd529a725dd199fe13546f7f32c35512b362e3b
Alraku <ekoalraku@gmail.com>
2022-11-18 19:43:10 +0100

    Introduced first testcase

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,20 @@
 const LoginPage = require('../pageobjects/login.page');
 const SecurePage = require('../pageobjects/secure.page');
+import { generatePassword, createUserCredentials } from '../../helpers';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
+
+describe('Register user to online store', () => {
+    it('should register new user account', async () => {
+        let user = createUserCredentials()
         await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        await LoginPage.signUp(user.email, user.password);
+        await LoginPage.isAlert()
+        const alertText = await browser.getAlertText();
+        expect(alertText).toEqual('Sign up successful.')
     });
 });
 
 
+// await expect(SecurePage.flashAlert).toBeExisting();
+// await expect(SecurePage.flashAlert).toHaveTextContaining(
+//     'You logged into a secure area!');
