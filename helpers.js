f71abe970af8a00849e6e458193cd7f58edfd850
Alraku <ekoalraku@gmail.com>
2022-11-20 12:26:56 +0100

    Added card generator, new properties of user, moved constants away from helpers

--- helpers.js
+++ helpers.js
@@ -1,15 +1,38 @@
 const crypto = require('crypto')
-
-const firstNameList = ['John', 'Frank', 'Mia', 'Ola'];
-const lastNameList = ['Green', 'Dunder', 'Cox', 'Raviq'];
+const generator = require('creditcard-generator')
+const { cityList,
+        countryList,
+        firstNameList,
+        lastNameList } = require('../webdriverio/constants')
 
 class User {
 
     constructor(firstName, lastName, email, password) {
-        this.name = firstName;
-        this.lastName = lastName
-        this.email = email
-        this.password = password
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+        this.location = new Location()
+        this.creditCard = new CreditCard();
+    }
+}
+
+class CreditCard {
+
+    constructor() {
+        let random = uniqueRandom(12) + 1;
+
+        this.number = generator.GenCC()[0];
+        this.month = String(random).padStart(2, '0');
+        this.year = '2025';
+    }
+}
+
+class Location {
+
+    constructor() {
+        this.city = cityList[uniqueRandom(4)]
+        this.country = countryList[uniqueRandom(2)]
     }
 }
 
@@ -25,7 +48,7 @@ function uniqueRandom(range) {
     return Math.floor(Math.random() * range)
 }
 
-export function createUserCredentials() {
+function createUser() {
     let firstName = firstNameList[uniqueRandom(3)]
     let lastName = lastNameList[uniqueRandom(3)]
     let number = uniqueRandom(100)
@@ -33,4 +56,4 @@ export function createUserCredentials() {
     return new User(firstName, lastName, email, generatePassword())
 }
 
-export { uniqueRandom }
+export { uniqueRandom, createUser }
